a9f946f4ffd7feb1527992f4264f2924cb4d7fd1
PhoenixEdward <phoenixjauregui@gmail.com>
2022-05-02 06:34:01 -0500

    titlescreen wireframe

--- script/title_screen/title_screen.gd
+++ script/title_screen/title_screen.gd
@@ -6,6 +6,15 @@ func _ready() -> void:
 	$AnimationPlayer.play("EaseIn")
 	$AnimationPlayer.connect("animation_finished", self, "_on_animation_finished")
 	get_tree().paused = true
-	
+
+
 func _on_animation_finished() -> void:
 	get_tree().paused = false
+
+
+func _on_play_button_pressed() -> void:
+	print("play button pressed")
+
+
+func _on_settings_button_pressed() -> void:
+	print("settings button pressed")
